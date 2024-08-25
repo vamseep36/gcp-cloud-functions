2b930b3576013a866ff7413855e37ec438f5aa17
Vishnu Vamsee Paningipalli <62656098+vamseep36@users.noreply.github.com>
2024-08-25 13:11:34 +0530

    Create index.php

---
+++ Hello-World/Php/index.php
@@ -0,0 +1,29 @@
+<?php
+use Google\CloudFunctions\FunctionsFramework;
+use Psr\Http\Message\ServerRequestInterface;
+
+// Register the function with Functions Framework.
+// This enables omitting the `FUNCTIONS_SIGNATURE_TYPE=http` environment
+// variable when deploying. The `FUNCTION_TARGET` environment variable should
+// match the first parameter.
+FunctionsFramework::http('helloHttp', 'helloHttp');
+
+function helloHttp(ServerRequestInterface $request): string
+{
+  $name = 'World';
+  $body = $request->getBody()->getContents();
+  if (!empty($body)) {
+    $json = json_decode($body, true);
+    if (json_last_error() != JSON_ERROR_NONE) {
+      throw new RuntimeException(sprintf(
+        'Could not parse body: %s',
+        json_last_error_msg()
+      ));
+    }
+    $name = $json['name'] ?? $name;
+  }
+  $queryString = $request->getQueryParams();
+  $name = $queryString['name'] ?? $name;
+
+  return sprintf('Hello, %s!', htmlspecialchars($name));
+}
